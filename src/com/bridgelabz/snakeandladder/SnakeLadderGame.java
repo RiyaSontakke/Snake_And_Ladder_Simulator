68616eac84629c51b6948f2d0258108e417a8c37
RiyaSontakke <riyaharhare04@gmail.com>
2023-03-16 13:36:29 +0530

    Added:Player starts at Position 0

--- src/com/bridgelabz/snakeandladder/SnakeLadderGame.java
+++ src/com/bridgelabz/snakeandladder/SnakeLadderGame.java
@@ -4,6 +4,7 @@ public class SnakeLadderGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int posA = 0;
 		System.out.println("Snake and Laddar Simulation Game");
 
 	}

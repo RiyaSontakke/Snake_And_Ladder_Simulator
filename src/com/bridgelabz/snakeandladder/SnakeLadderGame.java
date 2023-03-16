5093af6bbae18bbd44e86110b275ddb61c980805
Riya Sontakke <riyaharhare04@gmail.com>
2023-03-16 13:10:05 +0530

    Added:Snake and Ladder Game

---
+++ src/com/bridgelabz/snakeandladder/SnakeLadderGame.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeLadderGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Snake and Laddar Simulation Game");
+
+	}
+
+}

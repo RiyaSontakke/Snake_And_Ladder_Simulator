62da415d762789011efa4844ba6028f683f1d5ae
RiyaSontakke <riyaharhare04@gmail.com>
2023-03-16 15:41:31 +0530

    Added:Player gets Exact winning position 100

--- src/com/bridgelabz/snakeandladder/SnakeLadderGame.java
+++ src/com/bridgelabz/snakeandladder/SnakeLadderGame.java
@@ -4,8 +4,38 @@ public class SnakeLadderGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int posA = 0;
 		System.out.println("Snake and Laddar Simulation Game");
+		while (posA == 100) {
+		    int dieA = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		    int UpOrDownA = (int) Math.floor(Math.random() * 10) % 2 + 1;
+	        switch (UpOrDownA) {
+	        case 1:
+		             posA += dieA;
+		    break;
+
+	        case 2:
+		             if (posA <= 0) {
+							posA -= 0; 
+
+							System.out.println("Player A is Knocked Out . Need inside Game");
+
+						} else if (posA > 0) {
+
+							posA -= dieA;
+							if (posA < 0)
+								posA = 0;
+							break;
+						}
+	        default:
+	        	if (posA == 100) {
+					System.out.println("Game Over! Player A wins ");
+					return;
+				}
+	
+	     }
 
 	}
 
+}
 }

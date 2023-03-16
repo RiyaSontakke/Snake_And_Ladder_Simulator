25aa477d05c79fcb866299e5519b341e22aadc14
RiyaSontakke <riyaharhare04@gmail.com>
2023-03-16 14:05:17 +0530

    Added:Check For Options

--- src/com/bridgelabz/snakeandladder/SnakeLadderGame.java
+++ src/com/bridgelabz/snakeandladder/SnakeLadderGame.java
@@ -4,7 +4,21 @@ public class SnakeLadderGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int posA = 0;
 		System.out.println("Snake and Laddar Simulation Game");
+		int dieA = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		int UpOrDownA = (int) Math.floor(Math.random() * 10) % 2 + 1;
+	    switch (UpOrDownA) {
+	        case 1:
+		             posA += dieA; // ladder block
+		    break;
+
+	        case 2:
+		             posA -= dieA; // snake block
+		    break;
+	        default:
+	
+	     }
 
 	}
 
